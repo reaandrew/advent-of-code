78fef32063649eb76ce36941addb9db3ebd277ac
Andy Rea <email@andrewrea.co.uk>
2024-12-03 06:06:05 +0000

    Test returns sum of multiples ignoring corruption

--- 2024/3/tests.sh
+++ 2024/3/tests.sh
@@ -17,5 +17,9 @@ testReturnsSumOfMultiples(){
   assertEquals "2030" "$(answer "mul(44,46),mul(2,3)")"
 }
 
+testReturnsSumOfMultiplesIgnoringCorruption(){
+  assertEquals "161" "$(answer "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")"
+}
+
 # Load shUnit2.
 . shunit2
